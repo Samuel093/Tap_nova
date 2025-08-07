b4211df46fd97ae41d705c1369f8ec41d0c1b877
Samuel093 <areosamuel093@gmail.com>
2025-08-07 00:58:19 -0700

    add max-w template to p tag at hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,17 +8,17 @@ const Hero = () => {
       id="hero"
       className="bg-gradient-to-br from-indigo-50 to-white dark:from-gray-900 dark:to-gray-800"
     >
-      <div className="min-h-screen flex flex-col items-center justify-center px-4 py-14 md:px-8 md:py-24 lg:py-32 text-center">
+      <div className="min-h-[90vh] flex flex-col items-center justify-center px-4 py-14 md:px-8 md:py-22 lg:py-26 text-center">
         <motion.div
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
           className="max-w-xl md:max-w-2xl lg:max-w-4xl"
         >
-          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 text-gray-800 dark:text-white leading-tight md:leading-snug break-words">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mt-10 mb-6 text-gray-800 dark:text-white leading-tight md:leading-snug break-words">
             Empowering Brands With Innovative Solutions
           </h1>
-          <p className="text-base sm:text-lg md:text-xl text-gray-600 dark:text-gray-300 mb-8">
+          <p className="'max-w-130px mt-6 text-sm md:text-base text-gray-600 dark:text-gray-300 mb-8">
             TapNova helps businesses grow with technology, creativity, and strategic excellence.
           </p>
           <a
@@ -27,12 +27,12 @@ const Hero = () => {
           >
             Explore Services
           </a>
-        </motion.div>
-
-        {/* Infinite Logo Carousel */}
-        <div className="w-full overflow-x-hidden mt-10 mb-0">
+  {/* Infinite Logo Carousel */}
+        <div className="w-full overflow-x-hidden mt-10 ">
           <InfiniteLogoCarousel />
         </div>
+          
+        </motion.div>
       </div>
     </section>
   );
